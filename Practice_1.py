4083df98fb15079a0a01f4ddfa7be2b9a015f09b
Khushi Yadav <khushiyadav1254@gmail.com>
2026-06-01 11:12:45 +0530

    Add age input program with data type display
    The program takes age as input and prints the value entered along with its data type. It also converts the input to an integer and displays the new data type.

---
+++ Practice_1.py
@@ -0,0 +1,11 @@
+#question: program to take age as input and print value entered and its data type
+
+age = input("enter your age:")
+
+print("value entered is:",age)
+
+print(" data type is :",type(age))
+
+age = int(input("enter your age:"))
+
+print(" data type is :",type(age))
