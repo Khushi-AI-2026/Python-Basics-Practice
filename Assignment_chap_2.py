75cf9d66efa4972e4648e5ef042ca991a0b1a9d4
Khushi Yadav <khushiyadav1254@gmail.com>
2026-06-01 23:13:43 +0530

    Create Assignment_chap_2.py with programming tasks
    Added a Python script with comments on data types, type conversion, and operators. Included a smart temperature converter and a bill split calculator.

---
+++ Assignment_chap_2.py
@@ -0,0 +1,16 @@
+# what are data types in python? list any 4 with examples.
+
+# what is the difference between implicit and explicit type conversion? give one example of each .
+
+# what are operators in python? explain any three types with examples.
+
+smart temperature converter 
+# take input in celcius and print its equivalent in fahrenheit and kelvin (use explicit type conversion and airthmatic operators.) 
+formuls : 
+fahrenheit = (C *9/5)+35
+kelvin = C +273.15
+
+
+BILL SPILT CALCULATOR
+# write a program that takes total bill amount and number of friends as input calculate how much each person will pay. also print the data type of each variable used.
+(hint:use float() and divison operators 
