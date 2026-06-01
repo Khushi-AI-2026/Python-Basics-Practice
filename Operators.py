f7ab4e027374e1f9e59ba0dc00aa8e1aa2dcca15
Khushi Yadav <khushiyadav1254@gmail.com>
2026-06-01 22:59:04 +0530

    Add arithmetic, comparison, logical, and assignment operators

---
+++ Operators.py
@@ -0,0 +1,38 @@
+# Airthmatic operators
+
+x= 10
+y=5
+
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x%y)
+print(x**y)
+
+# Comparision operators
+
+print(x==y) # false
+print(x>=y) # true
+print(x<y) # false
+print(x>y) # true
+
+# Logical operators
+
+print("and operator result",x>y and x<y) # false
+print("or operator result",x>y or x<y) #true
+print("not operator result",not (x>y)) #false
+
+# Assignment operator
+
+a= 2
+b= 3
+a= a+6
+a+=6
+
+a= a-1
+a-=1
+
+a=a*2
+a*=2
+
