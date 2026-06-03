2534b7792379da62288fb96c2bc48237bf901b02
Khushi Yadav <khushiyadav1254@gmail.com>
2026-06-03 21:39:11 +0530

    Demonstrate tuple creation and immutability
    Added a new tuple and demonstrated tuple properties.

---
+++ 03_tuple.py
@@ -0,0 +1,13 @@
+a = (1,2,5,6)
+
+print(type(a))
+
+a= (3,34,567,6788, False,"rohan","mohan")
+a[0] = 453 #isko change nhi kr sakte tuple ko change nhi kr sakte 
+
+my_tuple = (1, 2, 3, "hello", 5.5)
+print(my_tuple)
+
+print(my_tuple[0])   # output: 1
+Tuple = fixed (unchangeable) collection of items in Python
+
