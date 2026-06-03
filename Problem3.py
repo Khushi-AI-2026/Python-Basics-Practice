0a52958fe69baff8e9bcf9179a96842fa095b274
Khushi Yadav <khushiyadav1254@gmail.com>
2026-06-03 12:58:49 +0530

    Add problem statements for Python exercises
    Added problem statements for external module usage and directory listing.

---
+++ Problem3.py
@@ -0,0 +1,5 @@
+#problem3: install an external module and use it to perform an operation of your interest 
+
+#problem4: write a python program to print the contents of a directory using the os module search online for thefunction which does that.
+
+#problem5: lable the program written in program 4 with comments 
