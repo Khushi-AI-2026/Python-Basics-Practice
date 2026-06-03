2f13a81deedfe2d73b61b087dcae234538acff6c
Khushi Yadav <khushiyadav1254@gmail.com>
2026-06-03 13:16:07 +0530

    Create Ch-2practice-st.py

---
+++ Ch-2practice-st.py
@@ -0,0 +1,10 @@
+#write a python program to add two numbers.
+
+#write a python program to find remainder when a number is divided by z
+#check the type of variables assigned using input() function.
+
+#use comparison operator to find out wheather a given variable a id greater or not .take a=34 and b= 80
+
+# write a program to find an average of two numbers entered bythr user.
+
+#write a python program to calculate the square of s number entered by user.
