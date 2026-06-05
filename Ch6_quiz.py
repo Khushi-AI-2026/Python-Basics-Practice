82723564bbca179ed102049c90342e172d022bf0
Khushi Yadav <khushiyadav1254@gmail.com>
2026-06-05 08:28:24 +0530

    Add age verification program

--- Ch6_quiz.py
+++ Ch6_quiz.py
@@ -1,3 +1,8 @@
 # write a program to print yes when the age entered by the user is greater than equal to 18
 
+age = int(input("Enter your age: "))
 
+if age >= 18:
+    print("Yes")
+else:
+    print("No")
