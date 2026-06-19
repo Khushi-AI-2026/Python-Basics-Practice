82a20fc68081679045638c693ecb2f6004ac6332
Khushi Yadav <khushiyadav1254@gmail.com>
2026-06-19 17:06:03 +0530

    Add program to check exam pass or fail status

---
+++ Variables-basic-Q.py
@@ -0,0 +1,6 @@
+#write a program to check your progress you are pass or fail
+marks = int(input("enter your marks in exam : "))
+if marks >= 33:
+     print("you are successful pass the exam, congratulations!")
+else :
+     print("you are fail,try again in next year")
